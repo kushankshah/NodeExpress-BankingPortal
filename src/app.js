ef120b4cbcb4381307ecbee030da9de5ac856bfc
kushankshah <50797490+kushankshah@users.noreply.github.com>
2019-05-19 20:30:34 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
@@ -12,35 +14,10 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended: true}));
 
-app.get('/savings', (req, res) => {
-   res.render('account', { account: accounts.savings});
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking});
- });
-
- app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit});
- }); 
-
 app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts}));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-
-app.post('/transfer', (req, res) => {
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit}));
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', {message: 'Payment Completed'});
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] });
